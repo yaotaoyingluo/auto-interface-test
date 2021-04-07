42108e2c9d13e49e5090c74bb9ef448dbf62b8b1
yaotao12 <11>
2021-04-07 17:09:14 +0800

    update:项目初始化

---
+++ src/main/java/com/yaotao/autointerfacetest/AutoInterfaceTestApplication.java
@@ -0,0 +1,13 @@
+package com.yaotao.autointerfacetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInterfaceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoInterfaceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaotao/autointerfacetest/AutoInterfaceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaotao.autointerfacetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoInterfaceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
